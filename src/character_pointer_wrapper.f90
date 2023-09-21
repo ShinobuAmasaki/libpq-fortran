d80f9a73b9cfdb1fa1c3326981fd518f3e4b10b5
Amasaki Shinobu <amasaki203@gmail.com>
2023-09-21 23:13:44 +0900

    [fix] function c_char_to_f_string.

--- src/character_pointer_wrapper.f90
+++ src/character_pointer_wrapper.f90
@@ -2,6 +2,7 @@ module character_pointer_wrapper
    implicit none
    private
    public :: c_to_f_charpointer
+   public :: c_char_to_f_string
 
 contains
 
@@ -12,7 +13,7 @@ contains
       type(c_ptr), intent(in) :: cptr
       character(:), allocatable, intent(out) :: str
 
-      character(:, kind=c_char), pointer :: fptr => null()
+      character(:, kind=c_char), allocatable :: buf
 
       integer(c_size_t) :: length
 
@@ -21,25 +22,44 @@ contains
          function strlen(ptr) bind(c)
             import c_ptr, c_size_t
             type(c_ptr), intent(in), value :: ptr
-            integer(c_size_t)  :: strlen
+            integer(c_size_t) :: strlen
          end function strlen
       end interface
 
       interface
-         subroutine free(ptr) bind(c)
+         subroutine pq_free(ptr) bind(c)
             import c_ptr
             type(c_ptr), intent(in), value :: ptr
-         end subroutine free
+         end subroutine pq_free
       end interface
 
       length = strlen(cptr)
 
-      fptr => convert_cptr(cptr, length)
+      call  convert_cptr(cptr, length, buf)
+      str = buf
 
-      str = fptr(1:length)
+      call pq_free(cptr)
 
-      call free(cptr)
-      nullify(fptr)
+
+   contains
+      ! `convert_cptr` takes a string with type(cptr) and its length as input,
+      ! and returns a corresponding Fortran pointer of character-type.
+      subroutine convert_cptr(cptr, length, buf)
+         use, intrinsic :: iso_c_binding, only: c_ptr, c_size_t, c_f_pointer, c_char
+         implicit none
+         type(c_ptr), intent(in) :: cptr
+         integer(c_size_t), intent(in) :: length
+         character(:, kind=c_char), allocatable, intent(out)  :: buf
+         
+         ! Declare a character-type pointer variable `fptr` with a length `length` and
+         ! a kind parameter `c_char`.
+         character(len=length, kind=c_char), pointer :: fptr
+
+         call c_f_pointer(cptr, fptr)
+
+         buf = fptr
+
+      end subroutine convert_cptr
 
    end subroutine c_char_to_f_string
 
@@ -64,23 +84,27 @@ contains
 
       ! Associate the pointer variable `res` with the result of the function `convert_cptr`.
       res => convert_cptr(char_cptr, strlen(char_cptr))
+
+   contains
+
+      ! `convert_cptr` takes a string with type(cptr) and its length as input,
+      ! and returns a corresponding Fortran pointer of character-type.
+      function convert_cptr(cptr, length) result(fptr)
+         use, intrinsic :: iso_c_binding, only: c_ptr, c_size_t, c_f_pointer, c_char
+         implicit none
+         type(c_ptr), intent(in) :: cptr
+         integer(c_size_t), intent(in) :: length
+
+         ! Declare a character-type pointer variable `fptr` with a length `length` and
+         ! a kind parameter `c_char`.
+         character(len=length, kind=c_char), pointer :: fptr
+
+         call c_f_pointer(cptr, fptr)
+
+      end function convert_cptr
+
    end function c_to_f_charpointer
       
-   ! `convert_cptr` takes a string with type(cptr) and its length as input,
-   ! and returns a corresponding Fortran pointer of character-type.
-   function convert_cptr(cptr, length) result(fptr)
-      use, intrinsic :: iso_c_binding, only: c_ptr, c_size_t, c_f_pointer, c_char
-      implicit none
-      type(c_ptr), intent(in) :: cptr
-      integer(c_size_t), intent(in) :: length
-
-      ! Declare a character-type pointer variable `fptr` with a length `length` and
-      ! a kind parameter `c_char`.
-      character(len=length, kind=c_char), pointer :: fptr
-
-      call c_f_pointer(cptr, fptr)
-
-   end function convert_cptr
 
 
 
