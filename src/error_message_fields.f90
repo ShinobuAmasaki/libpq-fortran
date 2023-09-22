6bac07ac34248f7c96996ebe2c64a1c1fc3aecb4
Amasaki Shinobu <amasaki203@gmail.com>
2023-09-22 17:47:26 +0900

    [mod] error_message_fields are defined as integers.

--- src/error_message_fields.f90
+++ src/error_message_fields.f90
@@ -1,24 +1,25 @@
 module error_message_fields
+   use, intrinsic :: iso_fortran_env
    implicit none
    private
 
-   character(1), parameter, public :: PG_DIAG_SEVERITY = 'S'
-   character(1), parameter, public :: PG_DIAG_SEVERITY_NONLOCALIZED = 'V'
-   character(1), parameter, public :: PG_DIAG_SQLSTATE = 'C'
-   character(1), parameter, public :: PG_DIAG_MESSAGE_PRIMARY = 'M'
-   character(1), parameter, public :: PG_DIAG_MESSAGE_DETAIL = 'D'
-   character(1), parameter, public :: PG_DIAG_MESSAGE_HINT = 'H'
-   character(1), parameter, public :: PG_DIAG_STATEMENT_POSITION = 'P'
-   character(1), parameter, public :: PG_DIAG_INTERNAL_POSITION = 'p'
-   character(1), parameter, public :: PG_DIAG_INTERNAL_QUERY = 'q'
-   character(1), parameter, public :: PG_DIAG_CONTEXT = 'W'
-   character(1), parameter, public :: PG_DIAG_SCHEMA_NAME = 's'
-   character(1), parameter, public :: PG_DIAG_TABLE_NAME = 't'
-   character(1), parameter, public :: PG_DIAG_COLUMN_NAME = 'c'
-   character(1), parameter, public :: PG_DIAG_DATATYPE_NAME = 'd'
-   character(1), parameter, public :: PG_DIAG_CONSTRAINT_NAME = 'n'
-   character(1), parameter, public :: PG_DIAG_SOURCE_FILE = 'F'
-   character(1), parameter, public :: PG_DIAG_SOURCE_LINE = 'L'
-   character(1), parameter, public :: PG_DIAG_SOURCE_FUNCTION = 'R'
+   integer(int32), parameter, public :: PG_DIAG_SEVERITY = ichar('S')
+   integer(int32), parameter, public :: PG_DIAG_SEVERITY_NONLOCALIZED = ichar('V')
+   integer(int32), parameter, public :: PG_DIAG_SQLSTATE = ichar( 'C')
+   integer(int32), parameter, public :: PG_DIAG_MESSAGE_PRIMARY = ichar('M')
+   integer(int32), parameter, public :: PG_DIAG_MESSAGE_DETAIL = ichar('D')
+   integer(int32), parameter, public :: PG_DIAG_MESSAGE_HINT = ichar('H')
+   integer(int32), parameter, public :: PG_DIAG_STATEMENT_POSITION = ichar('P')
+   integer(int32), parameter, public :: PG_DIAG_INTERNAL_POSITION = ichar('p')
+   integer(int32), parameter, public :: PG_DIAG_INTERNAL_QUERY = ichar('q')
+   integer(int32), parameter, public :: PG_DIAG_CONTEXT = ichar('W')
+   integer(int32), parameter, public :: PG_DIAG_SCHEMA_NAME = ichar('s')
+   integer(int32), parameter, public :: PG_DIAG_TABLE_NAME = ichar('t')
+   integer(int32), parameter, public :: PG_DIAG_COLUMN_NAME = ichar('c')
+   integer(int32), parameter, public :: PG_DIAG_DATATYPE_NAME = ichar('d')
+   integer(int32), parameter, public :: PG_DIAG_CONSTRAINT_NAME = ichar('n')
+   integer(int32), parameter, public :: PG_DIAG_SOURCE_FILE = ichar('F')
+   integer(int32), parameter, public :: PG_DIAG_SOURCE_LINE = ichar('L')
+   integer(int32), parameter, public :: PG_DIAG_SOURCE_FUNCTION = ichar('R')
 
 end module error_message_fields
