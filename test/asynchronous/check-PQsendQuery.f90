357c5b2974871852b02042c25894eaeb19932ed0
Amasaki Shinobu <amasaki203@gmail.com>
2023-09-30 16:07:47 +0900

    [update] PQsendQuery passes a test successfully.

---
+++ test/asynchronous/check-PQsendQuery.f90
@@ -0,0 +1,45 @@
+program main
+   use :: libpq
+   use :: iso_c_binding
+
+   type(c_ptr) :: conn, res
+
+   character(:, kind=c_char), allocatable :: conninfo, command
+
+   integer :: ires
+
+   print '(a)', "=== BEGIN TEST: PQsendQuery ==="
+   
+   conninfo = "host=localhost user=postgres dbname=postgres"
+
+   conn = PQconnectdb(conninfo)
+   if (PQstatus(conn) /= 0) then
+      print *, PQerrorMessage(conn)
+      call PQfinish(conn)
+      error stop
+   end if
+
+   command = "select 1234;"
+
+   ires = PQsendQuery(conn, command)
+
+   res = PQgetResult(conn)
+
+   do while (c_associated(res))
+
+      if (PQresultStatus(res) /= PGRES_TUPLES_OK) then
+         print *, PQerrorMessage(conn)
+      end if
+
+      print *, PQgetvalue(res, 0, 0)
+   
+      res = PQgetResult(conn)
+
+   end do
+      
+   call PQclear(res)
+   call PQfinish(conn)
+
+
+   print '(a)', "===== END TEST ====="
+end program main
